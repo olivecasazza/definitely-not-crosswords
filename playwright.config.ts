17e28d790097cf2580de832c6477bc0ee4a41b0d
Olive Casazza <olive.casazza@gmail.com>
2026-05-26 14:36:48 +0000

    feat(testing): integrate @nuxt/test-utils/playwright and configure Nitro external @prisma/client to prevent named export error

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests/e2e',
@@ -8,19 +8,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3011',
     trace: 'on-first-retry',
     browserName: 'chromium',
     launchOptions: {
       executablePath: '/home/olive/.nix-profile/bin/google-chrome-stable',
     },
-  },
-  webServer: {
-    command: 'node node_modules/nuxt/bin/nuxt.mjs dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    stdout: 'ignore',
-    stderr: 'pipe',
-    timeout: 120000,
+    nuxt: {
+      rootDir: __dirname,
+    },
   },
 })
